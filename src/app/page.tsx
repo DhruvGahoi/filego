18ba61ae64b5a4099c26a17853fabfb293172dd2
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-08-09 23:32:46 +0530

    frontend done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components_/Hero"
+import Navbar from "@/components_/Navbar"
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      FileGo
+    <div className="items-center justify-center h-[100vh] bg-white dark:bg-black">
+      <Navbar />
+      <Hero />
     </div>
-  );
+  )
 }
